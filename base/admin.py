5a7855bf0b26c031d804a0b6b1761e6ccd8336a4
bekmuxtorov <asadbekmuxtorov2@gmail.com>
2023-01-17 12:36:32 +0500

    ✒️  Added Topic, Room, Message models

--- base/admin.py
+++ base/admin.py
@@ -1,3 +1,21 @@
 from django.contrib import admin
+from . import models
 
 # Register your models here.
+
+
+class RoomAdmin(admin.ModelAdmin):
+    list_display = ['id', 'name', 'topic', 'update', 'created']
+
+
+class TopicAdmin(admin.ModelAdmin):
+    list_display = ['id', 'name']
+
+
+class MessageAdmin(admin.ModelAdmin):
+    list_display = ['id', 'user', 'room', 'created']
+
+
+admin.site.register(models.Room, RoomAdmin)
+admin.site.register(models.Topic, TopicAdmin)
+admin.site.register(models.Message, MessageAdmin)
